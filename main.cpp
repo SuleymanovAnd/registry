62cf332bb76cc5ed35af684e6467fcdbc2940ba9
Suleymanov Andrey <sasus37@gmail.com>
2022-08-02 18:24:03 +0300

    Done

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <map>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::map <std::string,std::string> registry;
+    std::map<std::string,std::string> :: iterator it;
+    int n = 0;
+    while (registry.size () < 10) {
+        std::cout << "<-";
+        std::string temp;
+        std::cin >> temp;
+
+        if (temp == "Next" ) {
+            std::cout << it  -> first << std::endl;
+           if(it!=registry.end()) {registry.erase(it);it = registry.begin();}
+        } else {
+            registry.insert(std::make_pair(temp,temp));
+            it = registry.begin () ;
+           n++;
+        }
+    }
 }
